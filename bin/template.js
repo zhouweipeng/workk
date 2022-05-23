22d05ffd71716682c04a4726662b78bf26aa296d
zhouweipeng <1195950166@qq.com>
2022-05-24 02:16:59 +0800

    feat: 增加显示数据来源

--- bin/template.js
+++ bin/template.js
@@ -6,5 +6,6 @@ export const workhour = [
   "法定节假: $holiday",
   "法定补班: $supwork",
   "正常周末: $weekend",
-  "总工时: $total"
+  "总工时: $total",
+  "数据来源: https://cn.bing.com/search?q=%E6%97%A5%E5%8E%86"
 ];
